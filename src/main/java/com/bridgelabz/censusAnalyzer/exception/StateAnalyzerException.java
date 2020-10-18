5169be8daca4594c6f6da0227705cb6706e09cea
Tanmay Jain <mail.tanmay.jain@gmail.com>
2020-10-18 17:59:23 +0530

    Add: Custom Exception class

---
+++ src/main/java/com/bridgelabz/censusAnalyzer/exception/StateAnalyzerException.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.censusAnalyzer.exception;
+
+public class StateAnalyzerException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3369988279518561290L;
+
+	public enum ExceptionType{
+		INVALID_FILE_PATH}
+	
+	public ExceptionType type;
+	
+	public StateAnalyzerException(String message,ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
