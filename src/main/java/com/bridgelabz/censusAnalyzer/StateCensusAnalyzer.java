80c1a712692876b57118407904ee6d03cc6f0a2a
Tanmay Jain <mail.tanmay.jain@gmail.com>
2020-10-18 19:12:34 +0530

    Refactor: Throw custom exception for invalid class type exception

--- src/main/java/com/bridgelabz/censusAnalyzer/StateCensusAnalyzer.java
+++ src/main/java/com/bridgelabz/censusAnalyzer/StateCensusAnalyzer.java
@@ -24,12 +24,20 @@ public class StateCensusAnalyzer {
 						ExceptionType.INVALID_FILE_PATH);
 			}
 			Reader reader=Files.newBufferedReader(Paths.get(FilePath));
-			CsvToBean<CSVStateCensus> csvToBean =
-					new CsvToBeanBuilder<CSVStateCensus>(reader)
-					.withIgnoreLeadingWhiteSpace(true)
-					.withSkipLines(1)
-					.withType(CSVStateCensus.class).build();
+			CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder =
+					new CsvToBeanBuilder<CSVStateCensus>(reader);
+			
+			try {
+				csvToBeanBuilder
+				.withIgnoreLeadingWhiteSpace(true)
+				.withSkipLines(1)
+				.withType(CSVStateCensus.class);
+			}catch(IllegalStateException exception){
+				throw new StateAnalyzerException("Invalid Class Type.",
+						ExceptionType.INVALID_CLASS_TYPE);
+			}
 
+			CsvToBean<CSVStateCensus> csvToBean=csvToBeanBuilder.build();
 			Iterator<CSVStateCensus> csvIterator = csvToBean.iterator();
 
             while (csvIterator.hasNext()) {
