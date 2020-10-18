fb2eb437a4b06d427f589b9500ad96bfa625fff4
Tanmay Jain <mail.tanmay.jain@gmail.com>
2020-10-18 20:23:29 +0530

    Add: Test Case for invalid delimitor

--- src/test/java/com/bridgelabz/censusAnalyzer/StateCensusAnalyzerTest.java
+++ src/test/java/com/bridgelabz/censusAnalyzer/StateCensusAnalyzerTest.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.censusAnalyzer;
 
 import static org.junit.Assert.*;
-import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,7 +9,8 @@ import com.bridgelabz.censusAnalyzer.exception.StateAnalyzerException;
 public class StateCensusAnalyzerTest {
 	
 	private final String CENSUS_CSV_PATH="./src/Resources/IndiaStateCensusData.csv";
-	private final String INAVLID_CENSUS_CSV_PATH="IndiaStateCensusData.csv";
+	private final String INVALID_CENSUS_CSV_PATH="IndiaStateCensusData.csv";
+	private final String INVALID_CENSUS_CSV_DELIM="./src/Resources/IndiaStateCensusDataInvalidDelim.csv";
 	private StateCensusAnalyzer analyser;
 	
 	@Before
@@ -21,16 +21,26 @@ public class StateCensusAnalyzerTest {
 	@Test
 	public void givenCensusCSVFileReturnsCorrectNoOfEntries() throws StateAnalyzerException {
 		int stateCount = analyser.readCSVData(CENSUS_CSV_PATH);
-		assertEquals(29, stateCount);
+		assertEquals(28, stateCount);
 	}
 	
 	@Test
 	public void givenIncorrectCSVFilePath_ThrowsCustomExceptionInvalidFilePath(){
 		try {
-			analyser.readCSVData(INAVLID_CENSUS_CSV_PATH);
+			analyser.readCSVData(INVALID_CENSUS_CSV_PATH);
 		} catch (StateAnalyzerException e) {
 			e.printStackTrace();
 			assertEquals(StateAnalyzerException.ExceptionType.INVALID_FILE_PATH, e.type);
+		}
+	}
+	
+	@Test
+	public void givenIncorrectDelimiter_ThrowsCustomExceptionInvalidDelimiter(){
+		try {
+			analyser.readCSVData(INVALID_CENSUS_CSV_DELIM);
+		} catch (StateAnalyzerException e) {
+			e.printStackTrace();
+			assertEquals(StateAnalyzerException.ExceptionType.INVALID_DELIM, e.type);
 		}
 	}
 }
