9ee40ee7c15a1aaf6518c678e0fe07ae66ffe133
Tanmay Jain <mail.tanmay.jain@gmail.com>
2020-10-18 18:20:24 +0530

    Add: Test case For Invalid Path

--- src/test/java/com/bridgelabz/censusAnalyzer/StateCensusAnalyzerTest.java
+++ src/test/java/com/bridgelabz/censusAnalyzer/StateCensusAnalyzerTest.java
@@ -5,8 +5,12 @@ import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.bridgelabz.censusAnalyzer.exception.StateAnalyzerException;
+
 public class StateCensusAnalyzerTest {
 	
+	private final String CENSUS_CSV_PATH="./src/Resources/IndiaStateCensusData.csv";
+	private final String INAVLID_CENSUS_CSV_PATH="IndiaStateCensusData.csv";
 	private StateCensusAnalyzer analyser;
 	
 	@Before
@@ -15,8 +19,18 @@ public class StateCensusAnalyzerTest {
 	}
 	
 	@Test
-	public void givenCensusCSVFile_ReturnsCorrectNoOfEntries() throws IOException {
-		int stateCount = analyser.readCSVData();
+	public void givenCensusCSVFileReturnsCorrectNoOfEntries() throws StateAnalyzerException {
+		int stateCount = analyser.readCSVData(CENSUS_CSV_PATH);
 		assertEquals(29, stateCount);
+	}
+	
+	@Test
+	public void givenIncorrectCSVFilePath_ThrowsCustomExceptionInvalidFilePath(){
+		try {
+			analyser.readCSVData(INAVLID_CENSUS_CSV_PATH);
+		} catch (StateAnalyzerException e) {
+			e.printStackTrace();
+			assertEquals(StateAnalyzerException.ExceptionType.INVALID_FILE_PATH, e.type);
+		}
 	}
 }
