0041bc8910a12b2253ea8c37da11224c0ba86797
Tanmay Jain <mail.tanmay.jain@gmail.com>
2020-10-19 20:42:47 +0530

    Refactor: Enforced DRY and changed method to generic

--- src/main/java/com/bridgelabz/censusAnalyzer/StateCensusAnalyzer.java
+++ src/main/java/com/bridgelabz/censusAnalyzer/StateCensusAnalyzer.java
@@ -1,6 +1,5 @@
 package com.bridgelabz.censusAnalyzer;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -9,58 +8,25 @@ import java.util.Iterator;
 import java.util.stream.StreamSupport;
 import com.bridgelabz.censusAnalyzer.exception.StateAnalyzerException;
 import com.bridgelabz.censusAnalyzer.exception.StateAnalyzerException.ExceptionType;
-import com.bridgelabz.censusAnalyzer.model.CSVStateCensus;
-import com.bridgelabz.censusAnalyzer.model.CSVStates;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class StateCensusAnalyzer {
 
-	public int readStateCensusCSVData(String FilePath) throws StateAnalyzerException {
+	public <T> int readCSVData(String FilePath,Class<T> className) throws StateAnalyzerException {
 		try {
 			Files.newBufferedReader(Paths.get(FilePath));
 			Reader reader=Files.newBufferedReader(Paths.get(FilePath));
-			CsvToBean<CSVStateCensus> csvToBean =
-					new CsvToBeanBuilder<CSVStateCensus>(reader)
+			CsvToBean<T> csvToBean =
+					new CsvToBeanBuilder<T>(reader)
 					.withIgnoreLeadingWhiteSpace(true)
-					.withSkipLines(1)
-					.withType(CSVStateCensus.class).build();
+					.withType(className).build();
 			
-			Iterator<CSVStateCensus> csvIterator = csvToBean.iterator();
+			Iterator<T> csvIterator = csvToBean.iterator();
 
-			Iterable<CSVStateCensus> csvItrable= () -> csvIterator;
+			Iterable<T> csvItrable= () -> csvIterator;
 			int count=(int) StreamSupport.stream(csvItrable.spliterator(),false)
 					.count();
-
-			BufferedReader br = new BufferedReader(reader);
-			
-			//Check Header
-			while (br.readLine()!=null) {
-				String[] head=br.readLine().split(",");
-				boolean flagCorrectHead=
-						head[0]=="State" &&
-						head[1]=="Population" &&
-						head[2]=="AreaInSqKm" &&
-						head[3]=="DensityPerSqKm";
-				if (!flagCorrectHead) {
-					throw new StateAnalyzerException("Invalid Headers",
-							ExceptionType.INVALID_HEAD);
-				}
-				break;
-			}
-			
-			
-			//Check delimitor
-			boolean flagCorrectDelim=true;
-			while (br.readLine()!=null) {
-				if (!br.readLine().contains(",")) {
-					flagCorrectDelim=false;
-				}
-			}
-			if (!flagCorrectDelim) {
-				throw new StateAnalyzerException("Invalid Delimitor",
-						ExceptionType.INVALID_DELIM);
-			}
 			
 			return count;
 
@@ -70,62 +36,9 @@ public class StateCensusAnalyzer {
 		}catch(IllegalStateException exception){
 			throw new StateAnalyzerException("Invalid Class Type.",
 					ExceptionType.INVALID_CLASS_TYPE);
-		}
-	}
-	
-	public int readStateCodeCSVData(String FilePath) throws StateAnalyzerException {
-		
-		try {
-			Files.newBufferedReader(Paths.get(FilePath));
-			Reader reader=Files.newBufferedReader(Paths.get(FilePath));
-			CsvToBean<CSVStates> csvToBean =
-					new CsvToBeanBuilder<CSVStates>(reader)
-					.withIgnoreLeadingWhiteSpace(true)
-					.withSkipLines(1)
-					.withType(CSVStates.class).build();
-			
-			Iterator<CSVStates> csvIterator = csvToBean.iterator();
-
-			Iterable<CSVStates> csvItrable= () -> csvIterator;
-			int count=(int) StreamSupport.stream(csvItrable.spliterator(),false)
-					.count();
-			
-			BufferedReader br = new BufferedReader(reader);
-			
-			//Check Header
-			while (br.readLine()!=null) {
-				String[] head=br.readLine().split(",");
-				boolean flagCorrectHead=
-						head[0]=="SrNo" &&
-						head[1]=="State Name" &&
-						head[2]=="TIN" &&
-						head[3]=="StateCode";
-				if (!flagCorrectHead) {
-					throw new StateAnalyzerException("Invalid Headers",
-							ExceptionType.INVALID_HEAD);
-				}
-				break;
-			}
-			
-			//Check delimitor
-			boolean flagCorrectDelim=true;
-			while (br.readLine()!=null) {
-				if (!br.readLine().contains(",")) {
-					flagCorrectDelim=false;
-				}
-			}
-			if (!flagCorrectDelim) {
-				throw new StateAnalyzerException("Invalid Delimitor",
-						ExceptionType.INVALID_DELIM);
-			}
-			
-			return count;
-		}catch(IOException exception) {
-			throw new StateAnalyzerException("Invalid Path Name",
-					ExceptionType.INVALID_FILE_PATH);
-		}catch(IllegalStateException exception){
-			throw new StateAnalyzerException("Invalid Class Type.",
-					ExceptionType.INVALID_CLASS_TYPE);
+		}catch(RuntimeException exception){
+			throw new StateAnalyzerException("Invalid Delimitor/Head Name",
+					ExceptionType.INVALID_DELIM_OR_HEAD);
 		}
 	}
 }
