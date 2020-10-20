3b7c714f6805622422fa91989540aed5ba032e42
Tanmay Jain <mail.tanmay.jain@gmail.com>
2020-10-20 19:08:12 +0530

    Removed: Duplicate get count func

--- src/main/java/com/bridgelabz/censusAnalyzer/csvbuilder/CSVBuilder.java
+++ src/main/java/com/bridgelabz/censusAnalyzer/csvbuilder/CSVBuilder.java
@@ -2,7 +2,7 @@ package com.bridgelabz.censusAnalyzer.csvbuilder;
 
 import java.io.Reader;
 import java.util.Iterator;
-import java.util.stream.StreamSupport;
+import java.util.List;
 
 import com.bridgelabz.censusAnalyzer.csvbuilder.exception.CSVBuilderException;
 import com.bridgelabz.censusAnalyzer.csvbuilder.exception.CSVBuilderException.ExceptionType;
@@ -11,15 +11,16 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 public class CSVBuilder<T> implements ICSVBuilder<T> {
 	
-	public int getCount(Reader reader,Class className) throws CSVBuilderException {
-		Iterator<T> csvIterator=this.getCSVIterator(reader, className);
-		Iterable<T> csvItrable= () -> csvIterator;
-		int count=(int) StreamSupport.stream(csvItrable.spliterator(),false)
-				.count();
-		return count;
+	public Iterator<T> getCSVIterator(Reader reader,Class className) throws CSVBuilderException{
+			return getCSVBean(reader, className).iterator();
+	}
+
+	@Override
+	public List<T> getCSVList(Reader reader, Class className) throws CSVBuilderException {
+		return getCSVBean(reader, className).parse();
 	}
 	
-	private Iterator<T> getCSVIterator(Reader reader,Class<T> className) throws CSVBuilderException {
+	private CsvToBean<T> getCSVBean(Reader reader,Class className) throws CSVBuilderException{
 		try {
 			CsvToBean<T> csvToBean =
 					new CsvToBeanBuilder<T>(reader)
@@ -27,7 +28,7 @@ public class CSVBuilder<T> implements ICSVBuilder<T> {
 					.withType(className)
 					.build();
 			
-			return csvToBean.iterator();
+			return csvToBean;
 		}catch(IllegalStateException exception){
 			throw new CSVBuilderException("Invalid Class Type.",
 					ExceptionType.INVALID_CLASS_TYPE);
